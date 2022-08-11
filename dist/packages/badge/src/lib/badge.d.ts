83c671dfe2ac3e00d7f61b053ab67d80a69ca5ab
Ubirajara Marsicano Neto <ubirajara_neto@hotmail.com>
2022-08-11 17:48:36 -0300

    chore(dist): update distributable

--- dist/packages/badge/src/lib/badge.d.ts
+++ dist/packages/badge/src/lib/badge.d.ts
@@ -25,9 +25,9 @@ interface BadgeOptions {
 declare type SecondaryMessages = Array<string>;
 declare function handleMouseEnter(event: Event): void;
 declare function handleMouseLeave(event: Event): void;
-declare const _handleMouseEnter = "function handleMouseEnter(el) {\n  const upperContainer = document.getElementById('upper-container');\n  upperContainer.classList.add(\"universal-badge-block\");\n  upperContainer.classList.remove(\"universal-badge-none\");\n}";
-declare const _handleMouseLeave = "function handleMouseLeave(el) {\n  const upperContainer = document.getElementById('upper-container');\n  upperContainer.classList.add(\"universal-badge-none\");\n  upperContainer.classList.remove(\"universal-badge-block\");\n}";
-declare const primaryMessage = "A\n    <a href=\"https://ae.studio/same-day-skunkworks\" target=\"_blank\">\n      SDS\n    </a>\n    Project \u2022 Made with &#10084; by\n    <a href=\"https://ae.studio/\" target=\"_blank\"> Agency Enterprise </a>";
+declare const _handleMouseEnter = "function handleMouseEnter(el) {\n  const upperContainer = document.getElementById('upper-container');\n  upperContainer.classList.add(\"sds-utils-badge-block\");\n  upperContainer.classList.remove(\"sds-utils-badge-none\");\n}";
+declare const _handleMouseLeave = "function handleMouseLeave(el) {\n  const upperContainer = document.getElementById('upper-container');\n  upperContainer.classList.add(\"sds-utils-badge-none\");\n  upperContainer.classList.remove(\"sds-utils-badge-block\");\n}";
+declare const primaryMessage = "A\n    <a href=\"https://ae.studio/same-day-skunkworks\" target=\"_blank\">SDS</a>\n    Project \u2022 Made with <img src=\"heart.gif\" alt=\"heart\" width=\"10\" height=\"10\"> by\n    <a href=\"https://ae.studio/\" target=\"_blank\"> Agency Enterprise </a>";
 declare const secondaryMessages: SecondaryMessages;
 declare const interFontSrc = "https://rsms.me/inter/inter.css";
 declare function SDSUtilsBadge({ location, position, theme, expandable, target, }: BadgeOptions): Promise<void>;

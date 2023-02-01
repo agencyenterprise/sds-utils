281be6e5cb66f47f4d6cd7e09d4d47c5e0cbfe47
Jean Mayer <jean.mayer@ae.studio>
2023-02-01 00:14:12 -0300

    feat: new design, tracking clicks

--- dist/packages/badge/src/lib/badge.js
+++ dist/packages/badge/src/lib/badge.js
@@ -67,21 +67,31 @@ function handleMouseLeave(event) {
 }
 var _handleMouseEnter = "function handleMouseEnter(el) {\n  const upperContainer = document.getElementById('upper-container');\n  upperContainer.classList.add(\"sds-utils-badge-block\");\n  upperContainer.classList.remove(\"sds-utils-badge-none\");\n}";
 var _handleMouseLeave = "function handleMouseLeave(el) {\n  const upperContainer = document.getElementById('upper-container');\n  upperContainer.classList.add(\"sds-utils-badge-none\");\n  upperContainer.classList.remove(\"sds-utils-badge-block\");\n}";
-var primaryMessage = "\n    Created with \u2665 by <a href=\"https://ae.studio/#sds-badge\" target=\"_blank\" class=\"utils-underline\">ae.studio's</a> <a href=\"https://ae.studio/same-day-skunkworks/#sds-badge\" class=\"utils-underline\" target=\"_blank\" rel=\"noreferrer\">Same Day Skunkworks</a>";
+var primaryMessage = "\n    <div style=\"display: flex; justify-content: space-between\"><a class=\"show-on-hover badge-lighter\" target=\"_blank\" rel=\"noreferrer\" onclick=\"trackClickSdsBadge('https://twitter.com/DailySkunkwork', 'Twitter')\" href=\"https://twitter.com/DailySkunkwork#sds-badge\">Twitter</a><div>Created with \u2665 by <a href=\"https://ae.studio/#sds-badge\" onclick=\"trackClickSdsBadge('https://ae.studio/#sds-badge', 'AE Studio')\" target=\"_blank\" class=\"utils-underline\">AE Studio</a></div></div>";
 var secondaryMessages = [
-    "We\u2019re on a mission to build something agency increasing (almost) every day.",
-    "<a href=\"https://ae.studio/same-day-skunkworks#sds-badge\" target=\"_blank\" rel=\"noreferrer\" class=\"utils-underline\">Learn more \u2192</a>",
+    "We build tools that increase human agency. <br />\n  Outrageously fast. <br /> <br />",
+    "<div class=\"badge-links\"><a href=\"https://ae.studio/same-day-skunkworks\" onclick=\"trackClickSdsBadge('https://ae.studio/same-day-skunkworks', 'Learn more \u2192')\" target=\"_blank\" rel=\"noreferrer\" class=\"utils-underline\">Learn more \u2192</a> <a class=\"badge-lighter\" onclick=\"trackClickSdsBadge('https://ae.studio/same-day-skunkworks#calendar', 'Work with AE Studio')\" href=\"https://ae.studio/same-day-skunkworks#calendar\" target=\"_blank\" rel=\"noreferrer\" class=\"utils-underline\">Work with AE Studio \u2192</a></div> <br /> ",
 ];
-var terciaryMessage = "<a href=\"https://twitter.com/DailySkunkwork#sds-badge\" target=\"_blank\" rel=\"noreferrer\" class=\"sds-twitter-link\"><svg fill=\"#000000\" xmlns=\"http://www.w3.org/2000/svg\"  viewBox=\"0 0 30 30\" width=\"15px\" height=\"15px\">    <path d=\"M28,6.937c-0.957,0.425-1.985,0.711-3.064,0.84c1.102-0.66,1.947-1.705,2.345-2.951c-1.03,0.611-2.172,1.055-3.388,1.295 c-0.973-1.037-2.359-1.685-3.893-1.685c-2.946,0-5.334,2.389-5.334,5.334c0,0.418,0.048,0.826,0.138,1.215 c-4.433-0.222-8.363-2.346-10.995-5.574C3.351,6.199,3.088,7.115,3.088,8.094c0,1.85,0.941,3.483,2.372,4.439 c-0.874-0.028-1.697-0.268-2.416-0.667c0,0.023,0,0.044,0,0.067c0,2.585,1.838,4.741,4.279,5.23 c-0.447,0.122-0.919,0.187-1.406,0.187c-0.343,0-0.678-0.034-1.003-0.095c0.679,2.119,2.649,3.662,4.983,3.705 c-1.825,1.431-4.125,2.284-6.625,2.284c-0.43,0-0.855-0.025-1.273-0.075c2.361,1.513,5.164,2.396,8.177,2.396 c9.812,0,15.176-8.128,15.176-15.177c0-0.231-0.005-0.461-0.015-0.69C26.38,8.945,27.285,8.006,28,6.937z\"/></svg> @DailySkunkwork</a>";
+var trackClickSdsBadge = function (url, btnText) {
+    fetch('https://www.samedayskunkworks.com/api/analytics/addEvent', {
+        method: 'post',
+        body: JSON.stringify({
+            origin: window.location.href,
+            destination: url,
+            event: "SDS Badge Click - ".concat(btnText),
+        }),
+    });
+};
 function SDSUtilsBadge(_a) {
     var location = _a.location, position = _a.position, _b = _a.theme, theme = _b === void 0 ? ThemeOptionsEnum.Light : _b, expandable = _a.expandable, target = _a.target;
     return __awaiter(this, void 0, void 0, function () {
-        var cssLink, analyticsScript, handleMouseEnterScript, handleMouseLeaveScript, targetElement, wrapper, upperContainer, i, upperContainerParagraph, terciaryMessageContainer, upperContainerDivider, wrapperParagraph;
+        var cssLink, analyticsScript, handleMouseEnterScript, handleMouseLeaveScript, targetElement, wrapper, upperContainer, i, upperContainerParagraph, upperContainerDivider, wrapperParagraph;
         return __generator(this, function (_c) {
             cssLink = document.createElement('link');
             cssLink.rel = 'stylesheet';
             cssLink.href =
-                'https://cdn.jsdelivr.net/gh/agencyenterprise/sds-utils@1.2.10/dist/packages/badge/src/lib/badge.css';
+                // 'https://cdn.jsdelivr.net/gh/agencyenterprise/sds-utils@latest/dist/packages/badge/src/lib/badge.css';
+                '/badge.css';
             document.head.appendChild(cssLink);
             analyticsScript = document.createElement('script');
             analyticsScript.src = 'https://scripts.simpleanalyticscdn.com/latest.js';
@@ -129,9 +139,6 @@ function SDSUtilsBadge(_a) {
                     upperContainerParagraph.innerHTML = secondaryMessages[i];
                     upperContainer.appendChild(upperContainerParagraph);
                 }
-                terciaryMessageContainer = document.createElement('p');
-                terciaryMessageContainer.innerHTML = terciaryMessage;
-                upperContainer.appendChild(terciaryMessageContainer);
                 upperContainerDivider = document.createElement('hr');
                 upperContainerDivider.classList.add('sds-utils-badge-opaque');
                 upperContainer.appendChild(upperContainerDivider);

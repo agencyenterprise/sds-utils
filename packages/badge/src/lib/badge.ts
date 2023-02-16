30551a251a9378fb7cf89ff68a642dd67f5ab7f4
Jean Mayer <jean.mayer@ae.studio>
2023-02-16 15:34:25 -0300

    feat: fix

--- packages/badge/src/lib/badge.ts
+++ packages/badge/src/lib/badge.ts
@@ -95,6 +95,7 @@ async function SDSUtilsBadge({
 
   footer.innerHTML = `
     <div class="sds-utils-footer-content">
+    <img src="./waves.gif" class="sds-utils-waves" />
       <div class="sds-utils-footer-inner-content">
         <a href="https://ae.studio/same-day-skunkworks" onClick="trackClickSdsBadge('https://ae.studio/same-day-skunkworks', 'Footer Logo')" target="_blank" rel="noreferrer">
         <svg viewBox="0 0 579 139" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -110,9 +111,21 @@ async function SDSUtilsBadge({
     </a>
     <div class="sds-utils-content-text">WE BUILD SOFTWARE, MACHINE LEARNING, AND BCI SOLUTIONS</div>
     <div class="sds-utils-content-text">HIRE THE WORLD’S MOST EFFECTIVE TEAM.</div>
-    <a href="https://ae.studio/same-day-skunkworks#calendar" onClick="trackClickSdsBadge('https://ae.studio/same-day-skunkworks#calendar', 'schedule a call')" target="_blank" rel="noreferrer"><div class="sds-utils-schedule-call">schedule a call</div></a>
+    <a class="sds-utils-schedule-call-a" href="https://ae.studio/same-day-skunkworks#calendar" onClick="trackClickSdsBadge('https://ae.studio/same-day-skunkworks#calendar', 'schedule a call')" target="_blank" rel="noreferrer"><div class="sds-utils-schedule-call">schedule a call</div></a>
     </div>
-    <div class="sds-utils-floating"><a href="https://ae.studio/same-day-skunkworks" onClick="trackClickSdsBadge('https://ae.studio/same-day-skunkworks', 'Discover more projects like this')" target="_blank" rel="noreferrer">Discover more projects like this -></a></div>
+    <div class="sds-utils-floating"><a href="https://ae.studio/same-day-skunkworks" onClick="trackClickSdsBadge('https://ae.studio/same-day-skunkworks', 'Discover more projects like this')" target="_blank" rel="noreferrer">Discover more projects like this <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+    viewBox="0 0 31.143 31.143" xml:space="preserve">
+ <g>
+   <g id="c100_arrow">
+     <path d="M0,15.571c0.001,1.702,1.383,3.081,3.085,3.083l17.528-0.002l-4.738,4.739c-1.283,1.284-1.349,3.301-0.145,4.507
+       c1.205,1.201,3.222,1.138,4.507-0.146l9.896-9.898c1.287-1.283,1.352-3.301,0.146-4.506c-0.033-0.029-0.068-0.051-0.1-0.08
+       c-0.041-0.043-0.07-0.094-0.113-0.139l-9.764-9.762c-1.268-1.266-3.27-1.316-4.474-0.111c-1.205,1.205-1.153,3.208,0.111,4.476
+       l4.755,4.754H3.085C1.381,12.485,0,13.865,0,15.571z"/>
+   </g>
+   <g id="Capa_1_46_">
+   </g>
+ </g>
+ </svg></a></div>
   <div>
     </div>
   `;

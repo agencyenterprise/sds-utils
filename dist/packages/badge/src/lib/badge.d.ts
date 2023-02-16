19f27d977a0295cd3602062bf688c694bec4cabb
Jean Mayer <jean.mayer@ae.studio>
2023-02-16 12:04:31 -0300

    feat: new version

--- dist/packages/badge/src/lib/badge.d.ts
+++ dist/packages/badge/src/lib/badge.d.ts
@@ -22,12 +22,5 @@ interface BadgeOptions {
     expandable?: boolean;
     target?: string;
 }
-declare type SecondaryMessages = Array<string>;
-declare function handleMouseEnter(event: Event): void;
-declare function handleMouseLeave(event: Event): void;
-declare const _handleMouseEnter = "function handleMouseEnter(el) {\n  const upperContainer = document.getElementById('upper-container');\n  upperContainer.classList.add(\"sds-utils-badge-block\");\n  upperContainer.classList.remove(\"sds-utils-badge-none\");\n}";
-declare const _handleMouseLeave = "function handleMouseLeave(el) {\n  const upperContainer = document.getElementById('upper-container');\n  upperContainer.classList.add(\"sds-utils-badge-none\");\n  upperContainer.classList.remove(\"sds-utils-badge-block\");\n}";
-declare const primaryMessage = "\n    <div style=\"display: flex; justify-content: space-between\"><a class=\"show-on-hover badge-lighter\" target=\"_blank\" rel=\"noreferrer\" onclick=\"trackClickSdsBadge('https://twitter.com/DailySkunkwork', 'Twitter')\" href=\"https://twitter.com/DailySkunkwork#sds-badge\">Twitter</a><div>Created with \u2665 by <a href=\"https://ae.studio/same-day-skunkworks\" onclick=\"trackClickSdsBadge('https://ae.studio/same-day-skunkworks', 'AE Studio')\" target=\"_blank\" class=\"utils-underline\">AE Studio</a></div></div>";
-declare const secondaryMessages: SecondaryMessages;
 declare const trackClickSdsBadge: (url: string, btnText: string) => void;
 declare function SDSUtilsBadge({ location, position, theme, expandable, target, }: BadgeOptions): Promise<void>;
